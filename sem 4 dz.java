58816db55719a68eb5ab8cf716107756ef28a24f
ibadovnikita <113236179+ibadovnikita@users.noreply.github.com>
2023-07-15 16:15:23 +0300

    Update sem 4 dz.java

--- sem 4 dz.java
+++ sem 4 dz.java
@@ -5,19 +5,12 @@ import java.util.Queue;
 
 public class homework_4 {
     public static void main(String[] args) {
-        /*Пусть дан LinkedList с несколькими элементами. 
-        Реализуйте метод, который вернет “перевернутый” список. */
-        List<String> list = createListString(); // создаем лист с рандомными значениями стринг
+  
         System.out.println(list);
         list = revers_List(list);
         System.out.println(list);
 
-        /*Реализуйте очередь с помощью LinkedList 
-        со следующими методами:
-        enqueue() - помещает элемент в конец очереди, 
-        dequeue() - возвращает первый элемент из очереди и удаляет его, 
-        first() - возвращает первый элемент из очереди, не удаляя. */
-
+   
         Queue<Integer> queue = new LinkedList<>();
         enqueue(queue, 5);
         System.out.println(queue);
@@ -35,8 +28,7 @@ public class homework_4 {
    
    static List<String> revers_List(List<String> array){
 
-    /*Пусть дан LinkedList с несколькими элементами. 
-    Реализуйте метод, который вернет “перевернутый” список. */ 
+   
     List<String> arr_result = new LinkedList<>();
     ListIterator listIterator = array.listIterator(array.size());
     while(listIterator.hasPrevious()){
